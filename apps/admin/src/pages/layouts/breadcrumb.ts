7b4ecdf08a34c5d126b09cdf9a5b3649991cc1bf
Öznur Özçelik <oznurozclk35@gmail.com>
2025-12-13 05:54:12 +0300

    Navbar' düzenlendi.

---
+++ apps/admin/src/pages/layouts/breadcrumb.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector:'app-breadcrumb',
+  imports: [RouterLink],
+  template:`
+  <ol class="breadcrumb"> 
+  <li class="breadcrumb-item">
+      <a routerLink="/" class="d-flex align-items-center">
+        <span class="material-symbols-outlined">home</span>
+         <span>Ana sayfa </span>
+      </a>
+  </li>
+  </ol> `,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class Breadcrumb {
+
+}
