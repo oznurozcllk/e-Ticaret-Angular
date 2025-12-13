574c4317d662badaf9b3c8530b81028197e8e9a7
Öznur Özçelik <oznurozclk35@gmail.com>
2025-12-13 06:51:00 +0300

    Sidebar düzenlendi

---
+++ apps/admin/src/navigation.ts
@@ -0,0 +1,18 @@
+export interface NavigationModel{
+    title:string;
+    url:string;
+    icon:string;
+}
+ 
+export const navigations: NavigationModel[]=[
+    {
+        title: "Ana Sayfa",
+        url:"/",
+        icon:"home"
+    },
+     {
+        title: "Ürünler",
+        url:"/products",
+        icon:"box"
+    }
+]
--- apps/admin/src/pages/layouts/layouts.ts
+++ apps/admin/src/pages/layouts/layouts.ts
@@ -1,13 +1,24 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal,  ViewEncapsulation } from '@angular/core';
 import Breadcrumb from './breadcrumb';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { navigations } from '../../navigation';
+import { NavPipe } from '../../pipes/nav-pipe';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [Breadcrumb,RouterLink],
+  imports: [Breadcrumb,
+    RouterLink,
+     RouterLinkActive,
+     CommonModule,
+     FormsModule,
+    NavPipe
+   ],
   templateUrl: './layouts.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class Layouts {
-
+readonly search = signal<string>("");
+  readonly navigations =computed(()=> navigations);
 }
