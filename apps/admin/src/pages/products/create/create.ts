f96479998d3708fe6787ca5ac2583e338102fae2
Öznur Özçelik <oznurozclk35@gmail.com>
2025-12-16 18:28:06 +0300

    Ürün kaydetme fonksiyonu ve API entegrasyonu eklendi.

--- apps/admin/src/pages/products/create/create.ts
+++ apps/admin/src/pages/products/create/create.ts
@@ -1,12 +1,27 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
 import Blank from '../../../components/blank';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+//import { Location } from '@angular/common';
 
 @Component({
-  imports: [Blank],
+  imports: [Blank, FormsModule],
   templateUrl: './create.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class ProductCreate {
+  readonly #http = inject(HttpClient);
+readonly #router=inject(Router);
+//readonly #location = inject(Location)
 
+save(form:NgForm){
+ if(!form.valid) return;
+ console.log(form.value);
+ this.#http.post("http://localhost:3000/products", form.value).subscribe(()=>{
+ this.#router.navigateByUrl("/products");
+//this.#location.back();
+ })
+}
 }
